ca844ed0d3311bc0933e9fcb135bd1e75569ab0a
cnduru <cnduru111@gmail.com>
2018-08-09 20:32:48 +0200

    Added timing capability.
    Dawn can now wake up at a set time and speak a greeting.

--- dawn.py
+++ dawn.py
@@ -1,26 +1,61 @@
 import weather
 import time
 import subprocess
+import threading
+
+SLEEP_INTERVAL = 15 # run every minute
+last_trigger_m = 0 # last trigger minute to eliminate multiple triggers within SLEEP_INTERVAL
+
+# strip time of leading zeros, if any
+def format_current_time(format24=False):
+    if(format24):
+        t = time.strftime("%H:%M:%S")
+    else:
+        t = time.strftime("%I:%M:%S")
+
+    t_hr = t.split(':')[0]
+    t_min = t.split(':')[1]
+
+    if t_hr[0] == '0':
+        t_hr = t_hr[1:]
+
+    if t_min[0] == '0':
+        t_min = t_min[1:]
+
+    return t_hr, t_min
+
+def should_trigger():
+    global last_trigger_m
+    trigger_h = '20'
+    trigger_m = '29'
+    h, m = format_current_time(format24=True)
+    
+    if h == trigger_h and m == trigger_m and trigger_m != last_trigger_m:
+        print('Trigger')
+        last_trigger_m = m # update last trigger minute
+        return True
+    else:
+        print(' no trigger {} {}'.format(h, m))
+    
 
 def speak(text):
     cmd = '/home/pi/bin/speak.sh \"{}\"'.format(text)
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
-weather.update_weather()
+def main():
+    weather.update_weather()
 
-t = time.strftime("%I:%M:%S")
-t_hr = t.split(':')[0]
-t_min = t.split(':')[1]
+    h, m = format_current_time()
 
-if t_hr[0] == '0':
-    t_hr = t_hr[1:]
-
-if t_min[0] == '0':
-    t_min = t_min[1:]
-
-greeting = """Good morning. The time is {} {} and it is time to wake up. 
-The weather is {} so im sure it will be a lovely day""".format(t_hr,
-                                                               t_min,
+    greeting = """Good morning. The time is {} {} and it is time to wake up. 
+The weather is {} so im sure it will be a lovely day""".format(h,
+                                                               m,
                                                                weather.last_weather)
-print greeting
-speak(greeting)
+    if should_trigger():
+        speak(greeting)
+        print(greeting)
+    
+    threading.Timer(SLEEP_INTERVAL, main).start()
+
+print('Dawn launched')
+main()
